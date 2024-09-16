762c7c27ae56745c27cfaedc03c4ad26c4e74830
Sonia <30045332@cc.peralta.edu>
2024-09-15 23:53:24 -0700

    Initial Commit

---
+++ class_activity10/main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  class_activity10
+//
+//  Created by Sonia Lopchan on 9/15/24.
+//Write a program that calculates the commission.
+
+/*use this chart to set the commission based on the value of the sales.
+
+sales    commission
+up to $10,000    10%
+$10,000 to $15,000     15%
+0ver $15,000    20%*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double sales, commission;
+    
+    cout <<"Enter the total sales value: ";
+    cin >> sales;
+    
+    if (double salesValue = 10000) {
+        <#statements#>
+    } else {
+        <#statements#>
+    }
+    
+    
+    
+    
+    return 0;
+}
